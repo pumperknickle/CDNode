05ecff59a42e414abe50e26550df4d7450de47d2
Pumper Knickle <pumperknickle@protonmail.com>
2019-08-18 15:11:06 -0700

    first commit

---
+++ Sources/App/Extensions/UInt256Extensions.swift
@@ -0,0 +1,7 @@
+import Foundation
+import CryptoStarterPack
+import Vapor
+
+extension UInt256: Parameter { }
+
+extension UInt256: Content { }
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,13 +1,27 @@
 import App
 import XCTest
+import Vapor
+import Redis
+import CryptoStarterPack
 
 final class AppTests: XCTestCase {
-    func testNothing() throws {
-        // Add your tests here
-        XCTAssert(true)
+    var app: Application!
+    let input = "Hello world".data(using: .utf8)!.toBoolArray()
+    var digest: [Bool]! { return HashDelegate.hash(input)! }
+    
+    override func setUp() {
+        app = try! Application.testable()
     }
-
-    static let allTests = [
-        ("testNothing", testNothing)
-    ]
+    
+    override func tearDown() {
+        try? app.syncShutdownGracefully()
+    }
+    
+    func testAPIPostAndGet() throws {
+        let response = try app.sendRequest(to: "put/" + input.literal(), method: .PUT)
+        XCTAssertTrue(response.http.status == .ok)
+        let otherResponse = try app.sendRequest(to: "get/" + digest.literal(), method: .GET)
+        XCTAssertTrue(otherResponse.http.status == .ok)
+        XCTAssertTrue(otherResponse.http.body.description == input.literal())
+    }
 }
